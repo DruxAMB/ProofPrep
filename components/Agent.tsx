ed06044ca50a6835b3e921d02ee0aaf1d0cab031
DruxAMB <druxambmarketing@gmail.com>
2025-05-05 13:29:31 +0100

    changed the user avatar

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 import { interviewer } from "@/constants";
 import { createFeedback } from "@/lib/actions/general.action";
+import { User } from "lucide-react";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
